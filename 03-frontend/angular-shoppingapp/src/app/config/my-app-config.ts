0d41bc39bd4e4bc8fd213b79923515f132ac00f1
liuxin-James <xl9jv@virginia.edu>
2022-08-01 20:24:43 -0400

    Update the logout/login auth

---
+++ 03-frontend/angular-shoppingapp/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa60zf7cm1GU6SOM5d7',
+        issuer: 'https://dev-82725437.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
